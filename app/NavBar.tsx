37384059f4858f35f580a608aa062cd51f19f42e
Noah <noahsanchez73@gmail.com>
2026-07-27 21:11:19 +0200

    [NavBar][ADD] Enhance NavBar - Add dynamic link styling - Add classnames dependency

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import { CiMoneyCheck1 } from "react-icons/ci";
+import classnames from 'classnames';
 
 
 const NavBar = () => {
+
+  const currentPath = usePathname();
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -15,7 +21,10 @@ const NavBar = () => {
       <ul className='flex space-x-6'>
         {links.map(link => 
           <Link key={link.href}
-          className='hover:text-zinc-500 transition-colors'
+          className={classnames({
+            'underline': link.href === currentPath,
+            'hover:text-zinc-500 transition-colors': true, 
+          })}
           href={link.href}>{link.label}</Link>
         )}
       </ul>
