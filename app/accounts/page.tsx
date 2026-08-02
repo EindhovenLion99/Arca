4464b2af06cbcda29c6995e6a002095807a59b8a
Noah <noahsanchez73@gmail.com>
2026-08-02 02:10:34 +0200

    [page][account] Set up accounts page - Added radix-ui package - Creating initial account page structure

---
+++ app/accounts/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from "@radix-ui/themes";
+
+const AccountPage = () => {
+  return (
+    <div><Button>New Account</Button></div>
+  )
+}
+
+export default AccountPage
