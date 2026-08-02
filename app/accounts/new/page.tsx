d099fe8c9d734bba618c0e405ee92c8e32331fca
Noah <noahsanchez73@gmail.com>
2026-08-02 14:32:39 +0200

    [page][account] Add New Account page and update layout padding

---
+++ app/accounts/new/page.tsx
@@ -0,0 +1,11 @@
+import { TextField } from "@radix-ui/themes"
+
+const NewAccountPage = () => {
+  return (
+    <div className="max-w-xl">
+      <TextField.Root placeholder="Title"/>
+    </div>
+  )
+}
+
+export default NewAccountPage
